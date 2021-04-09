af37f043c3f49e7a0f5fc08ece43679b413534d4
Robert Hollingworth <r.hollingworth@hotmail.com>
2021-04-09 22:23:21 +0100

    Added Windows UI project, configured with Caliburn DI

---
+++ BRMWindowsUI/Views/ShellView.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace BRMWindowsUI.Views {
+    public partial class ShellView : Window {
+        public ShellView() {
+            InitializeComponent();
+        }
+    }
+}
