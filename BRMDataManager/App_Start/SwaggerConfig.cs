02c38d28ca47bfdd2e4330a0f735fc9a528aecbe
Robert Hollingworth <r.hollingworth@hotmail.com>
2021-04-09 19:15:02 +0100

    Updated swagger to use auth token

--- BRMDataManager/App_Start/SwaggerConfig.cs
+++ BRMDataManager/App_Start/SwaggerConfig.cs
@@ -1,7 +1,9 @@
 using System.Web.Http;
 using WebActivatorEx;
 using BRMDataManager;
+using BRMDataManager.CustomSwagger;
 using Swashbuckle.Application;
+
 
 [assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
 
@@ -16,6 +18,8 @@ namespace BRMDataManager
             GlobalConfiguration.Configuration
                 .EnableSwagger(c =>
                     {
+                        c.DocumentFilter<AuthTokenOperation>();
+                        c.OperationFilter<AuthorisationOperationFilter>();
                         // By default, the service root url is inferred from the request used to access the docs.
                         // However, there may be situations (e.g. proxy and load-balanced environments) where this does not
                         // resolve correctly. You can workaround this by providing your own code to determine the root URL.
@@ -32,11 +36,11 @@ namespace BRMDataManager
                         // hold additional metadata for an API. Version and title are required but you can also provide
                         // additional fields by chaining methods off SingleApiVersion.
                         //
-                        c.SingleApiVersion("v1", "BRMDataManager");
+                        c.SingleApiVersion("v1", "Buggy Retail Manager API");
 
                         // If you want the output Swagger docs to be indented properly, enable the "PrettyPrint" option.
                         //
-                        //c.PrettyPrint();
+                        c.PrettyPrint();
 
                         // If your API has multiple versions, use "MultipleApiVersions" instead of "SingleApiVersion".
                         // In this case, you must provide a lambda that tells Swashbuckle which actions should be
@@ -143,7 +147,7 @@ namespace BRMDataManager
                         // enum type. Swashbuckle will honor this change out-of-the-box. However, if you use a different
                         // approach to serialize enums as strings, you can also force Swashbuckle to describe them as strings.
                         //
-                        //c.DescribeAllEnumsAsStrings();
+                        c.DescribeAllEnumsAsStrings();
 
                         // Similar to Schema filters, Swashbuckle also supports Operation and Document filters:
                         //
@@ -182,7 +186,7 @@ namespace BRMDataManager
                         // Use the "DocumentTitle" option to change the Document title.
                         // Very helpful when you have multiple Swagger pages open, to tell them apart.
                         //
-                        //c.DocumentTitle("My Swagger UI");
+                        c.DocumentTitle("Buggy API");
 
                         // Use the "InjectStylesheet" option to enrich the UI with one or more additional CSS stylesheets.
                         // The file must be included in your project as an "Embedded Resource", and then the resource's
